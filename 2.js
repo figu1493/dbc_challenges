d6dca2ab824392a6367c0681e17b25a177f1b262
John Figueiredo <jfigueir33@gmail.com>
2013-03-03 22:13:57 -0800

    2 and 3

---
+++ 2.js
@@ -0,0 +1,8 @@
+// Shuffle all the elements in an array. It had better be random...
+
+// Example usage:
+
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+shuffle(arr);
+// returns [9, 2, 8, 3, 4, 6, 10, 7, 5, 1]
