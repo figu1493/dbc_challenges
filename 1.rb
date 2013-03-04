5ed14201cbde446c61cf1cc250b2c0f41b919131
John Figueiredo <jfigueir33@gmail.com>
2013-03-03 22:02:35 -0800

    first commit

---
+++ 1.rb
@@ -0,0 +1,5 @@
+# var calc = new Calculator();
+
+# calc.evaluate('y x +');               // returns 'y x +'
+# calc.evaluate('3 1 x * +');           // returns '3 x +'
+# calc.evaluate('0 x +');               // returns 'x'
